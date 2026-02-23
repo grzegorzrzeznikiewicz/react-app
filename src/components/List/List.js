bfe05a57bc2e91877a48cf4fa81a68974addc07a
Grzegorz Rzeznikiewicz <grzegorzrzeznikiewicz@gmail.com>
2026-02-23 20:45:08 +0100

    Module 14.5: Add React components and styling for to-do app interface

---
+++ src/components/List/List.js
@@ -0,0 +1,18 @@
+import styles from './List.module.scss';
+import Column from '../Column/Column';
+
+const List = () => {
+    return (
+        <div>
+            <header className={styles.header}>Things to do<span>soon</span></header>
+            <p className={styles.description}>Interesting things I want to check out</p>
+            <section className={styles.columns}>
+                <Column title="Books" icon="book" />
+                <Column title="Movies" icon="film" />
+                <Column title="Games" icon="gamepad" />
+            </section>
+        </div>
+    );
+};
+
+export default List;
