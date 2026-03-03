e00663e7ab2179f7a2b2d080d6c40e66e70fec54
Grzegorz Rzeznikiewicz <grzegorzrzeznikiewicz@gmail.com>
2026-03-03 23:30:19 +0100

    Module 14.7: Add state management and dynamic functionality to to-do app components

---
+++ src/components/ColumnForm/ColumnForm.js
@@ -0,0 +1,27 @@
+import styles from './ColumnForm.module.scss';
+import { useState } from 'react';
+import Button from "../Button/Button";
+import TextInput from "../TextInput/TextInput";
+
+const ColumnForm = props => {
+    const [title, setTitle] = useState('');
+    const [icon, setIcon] = useState('');
+    const handleSubmit = e => {
+        e.preventDefault();
+        props.action({ title: title, icon: icon });
+        setTitle('');
+        setIcon('');
+    };
+
+    return (
+        <form className={styles.columnForm} onSubmit={handleSubmit}>
+            <label>Title: <TextInput value={title} onChange={e => setTitle(e.target.value)} /></label>
+            <label>Icon: <TextInput value={icon} onChange={e => setIcon(e.target.value)} /></label>
+            <Button>
+                Add column
+            </Button>
+        </form>
+    );
+};
+
+export default ColumnForm;
