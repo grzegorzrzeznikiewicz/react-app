79ca29bc858f4eb923a61fea6d78fac42070574c
Grzegorz Rzeznikiewicz <grzegorzrzeznikiewicz@gmail.com>
2026-04-12 22:17:22 +0200

    Module 16.3 - task 2: Refactor ADD_COLUMN action to use consistent payload structure and switch-case in reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,12 @@ import initialState from './initialState';
 import shortid from "shortid";
 
 const reducer = (state, action) => {
-    if (action.type === 'ADD_COLUMN') return {
-        ...state,
-        columns: [...state.columns, {...action.newColumn, id: shortid()}]
-    };
-    return state;
+    switch (action.type) {
+        case 'ADD_COLUMN':
+            return {...state, columns: [...state.columns, {...action.payload, id: shortid()}]};
+        default:
+            return state;
+    }
 };
 
 const store = createStore(
