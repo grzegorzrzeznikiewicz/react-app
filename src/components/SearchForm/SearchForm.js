8885c78a72508b0935aba20cdb395f44dfe8d818
Grzegorz Rzeznikiewicz <grzegorzrzeznikiewicz@gmail.com>
2026-04-16 08:59:54 +0200

    Module 16.4: Add search functionality with Redux integration and implement search filtering in Column component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
 
 const SearchForm = () => {
+    const [value, setValue] = useState('');
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch({type: 'UPDATE_SEARCHSTRING', payload: value});
+    };
     return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder="Search…" />
+        <form className={styles.searchForm} onSubmit={handleSubmit}>
+            <TextInput
+                placeholder="Search…"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+            />
             <Button>
                 <span className="fa fa-search" />
             </Button>
--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -16,7 +16,6 @@ const initialState = {
             icon: 'gamepad',
         }
     ],
-
     cards: [
         { id: 1, columnId: 1, title: 'This is Going to Hurt' },
         { id: 2, columnId: 1, title: 'Interpreter of Maladies' },
@@ -24,8 +23,8 @@ const initialState = {
         { id: 4, columnId: 2, title: 'Star Wars' },
         { id: 5, columnId: 3, title: 'The Witcher' },
         { id: 6, columnId: 3, title: 'Skyrim' }
-    ]
-
+    ],
+    searchString: '',
 };
 
 export default initialState;
