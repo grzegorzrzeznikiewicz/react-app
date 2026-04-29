4561def9a89694ecae901d56cfed112c3390e748
Grzegorz Rzeznikiewicz <grzegorzrzeznikiewicz@gmail.com>
2026-04-29 22:35:13 +0200

    Submodule 17.4: Add `Favorite` page functionality to display favorite cards using Redux store, update `Card` component props, and include `clsx` package

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,20 @@
 import styles from './Card.module.scss';
+import {useDispatch} from 'react-redux';
+import {toggleCardFavorite} from '../../redux/store';
+import clsx from 'clsx';
 
 const Card = props => {
+    const dispatch = useDispatch();
     return (
-        <li className={styles.card}>{props.title}</li>
+        <li className={styles.card}>
+            {props.title}
+            <button
+                className={clsx(styles.button, props.isFavorite && styles.isFavorite)}
+                onClick={() => dispatch(toggleCardFavorite(props.id))}
+            >
+                <i className="fa fa-star-o"/>
+            </button>
+        </li>
     );
 };
 
--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -1,11 +1,21 @@
 import styles from './Favorite.module.scss';
 import PageTitle from "../PageTitle/PageTitle";
+import {useSelector} from 'react-redux';
+import {getFavoriteCards} from '../../redux/store';
+import Card from '../Card/Card';
 
 const Favorite = () => {
+    const cards = useSelector(getFavoriteCards);
     return (
         <div className={styles.favorite}>
             <PageTitle>Favorite</PageTitle>
-            <p className={styles.subtitle}>Tu znajdziesz swoje ulubione karty.</p>
+            {cards.length === 0 ? (
+                <p>No cards...</p>
+            ) : (
+                <ul className={styles.cards}>
+                    {cards.map(card => <Card key={card.id} id={card.id} title={card.title} isFavorite={card.isFavorite}/>)}
+                </ul>
+            )}
         </div>
     );
 };
